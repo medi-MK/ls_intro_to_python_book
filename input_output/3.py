2322a228ca5282d9759065ef5f99183148706329
Medi <139905524+medi-MK@users.noreply.github.com>
2024-02-22 07:11:53 +0000

    added input_output chapter

---
+++ input_output/3.py
@@ -0,0 +1,9 @@
+age = input('How old are you? ')
+age = int(age)
+
+print(f'You are {age} years old.')
+
+print(f'In 10 years, you will be {age + 10} years old.')
+print(f'In 20 years, you will be {age + 20} years old.')
+print(f'In 30 years, you will be {age + 30} years old.')
+print(f'In 40 years, you will be {age + 40} years old.')
